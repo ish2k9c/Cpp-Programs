cb188dfd69e29a4997934010b208c0640d2fd34b
ish <ish2k9c@gmail.com>
2016-08-15 19:55:27 -0400

    first commit

---
+++ program1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// [A] - 1.1      Purpose of this code is to understand how to output information in the terminal using - [cout]
+// [B] - 1.2-1.3  How to end lines in different ways - [endl], [\n]
+// [C] - 1.4      How to end lines more than once ect.. - [\n]
+// [D] - 1.5      Using multiple end lines in a sentence - [\n]
+
+
+
+//where the code starts
+
+int main()
+{
+    // example 1.1
+    ///////////////////////////////////////////////////////////////////////////
+    cout << "Hello world!";
+
+    // example 1.2
+    ///////////////////////////////////////////////////////////////////////////
+    cout << "Hello world!" << endl;
+
+    // example 1.3
+    ///////////////////////////////////////////////////////////////////////////
+    cout << "Hello world!\n";
+
+    // example 1.4
+    ///////////////////////////////////////////////////////////////////////////
+    cout << "Hello world!\n\n";
+
+    // example 1.5
+    ///////////////////////////////////////////////////////////////////////////
+    cout << " Hello \n World \n !";
+    return 0;
+}
+
+//where the code ends
